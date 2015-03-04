cd82fe9b9390b08cacb9c2b1e072e35cf3fe9a19
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2015-03-04 13:41:15 +0000

    add missing definition of _INSTR_H_INCLUDED

--- src/emulator/instr.h
+++ src/emulator/instr.h
@@ -15,7 +15,8 @@
 // Boston, MA 02111-1307, USA
 
 
-#ifndef INSTR_H_INCLUDED
+#ifndef _INSTR_H_INCLUDED
+#define _INSTR_H_INCLUDED
 #ifndef FAST
 extern void print_instr(int /* op_field */, int /* arg_field */, u_int16_t * /* e_pc */);
 extern void print_pc(u_int16_t * /* e_pc */);
